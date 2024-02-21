c77dee09852eec460d41238ad82b2da33f0ba83c
Arman-Mishra <armanmishra.sky@gmail.com>
2024-02-22 02:41:30 +0530

    Added the hero section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/NavSection/Navbar/Navbar";
+import Hero from "./components/HeroSection/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
