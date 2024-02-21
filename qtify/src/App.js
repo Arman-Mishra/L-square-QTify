9e9d37e3533a3ab048eabb6e2f6c9ea62627de62
Arman-Mishra <armanmishra.sky@gmail.com>
2024-02-21 19:41:14 +0530

    C3:Button

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ButtonFeedback from "./components/ButtonFeedback";
+import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
     <div className="App">
       <ButtonFeedback />
+      <Searchbar />
     </div>
   );
 }
---
+++ qtify/src/components/ButtonFeedback.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./button.css";
+
+export default function ButtonFeedback() {
+  return (
+    <div>
+      <button className="Button">Give Feedback</button>
+    </div>
+  );
+}
